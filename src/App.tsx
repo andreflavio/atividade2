6d7d4970d64ed2c06714218638c36881faa35e59
andreflavio <13demaio198179@gmail.com>
2024-04-20 18:59:05 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/*
 import styled from "styled-components";
 import RGB from "./pages/RGB";
 import HSLA from "./pages/HSLA";
@@ -10,6 +11,34 @@ export default function App() {
       <HSLA />
       <CMYK />
     </WrapperSld>
+  );
+}
+
+const WrapperSld = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+*/
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from "styled-components";
+import RGB from "./pages/RGB";
+import HSLA from "./pages/HSLA";
+import CMYK from "./pages/CMYK";
+
+export default function App() {
+  return (
+    <Router>
+      <WrapperSld>
+        <Routes>
+          <Route path="/rgb" element={<RGB />} />
+          <Route path="/hsla" element={<HSLA />} />
+          <Route path="/cmyk" element={<CMYK />} />
+        </Routes>
+      </WrapperSld>
+    </Router>
   );
 }
 
